8302bf3ca139d289562bb04b08477c9086289135
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2023-12-18 23:11:19 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -172,8 +172,25 @@ if "image_path" in locals():
         st.error("Text extraction failed.")
 
 
-st.write("", size="small")
-st.link_button("GitHub repository", "https://github.com/meaviral17/LingualLens-Project")
-st.write("", size="small")
+def get_st_button_a_tag(url_link, button_name):
+    """
+    generate html a tag
+    :param url_link:
+    :param button_name:
+    :return:
+    """
+    return f'''
+    <a href={url_link}><button style="
+    fontWeight: 400;
+    padding: 0.25rem 0.75rem;
+    borderRadius: 0.25rem;
+    margin: 0px;
+    lineHeight: 1.6;
+    width: auto;
+    userSelect: none;
+    backgroundColor: #FFFFFF;
+    border: 1px solid rgba(49, 51, 63, 0.2);">{button_name}</button></a>
+    '''
+st.markdown(get_st_button_a_tag('https://github.com/meaviral17/LingualLens-Project', 'GitHub Repository'), unsafe_allow_html=True)
 st.write("Developed by Aviral Srivastava.", size="small")
 
