b7cee8041340fb8056ac21e0f8cef492eb13daf6
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2023-12-18 23:54:55 +0530

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -31,7 +31,7 @@ cus_css = """
         color: #333 !important;
         font-weight: bold !important;
         border-radius: 5px !important;
-        height: 20px !important; 
+        height: 10px !important; 
     }
     [data-testid="stAppViewContainer"] {
     background-color: #00008B;
